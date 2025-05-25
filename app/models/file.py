e0669b14d130d826b4861f39dd53b991546e3236
Hayden <107214489+Flodzz@users.noreply.github.com>
2025-05-25 18:25:40 +0100

    Use local sqlite database for now (#5)

---
+++ app/models/file.py
@@ -0,0 +1,12 @@
+from .db import db
+from datetime import datetime
+
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(255), nullable=False)
+    upload_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    file_path = db.Column(db.String(512), nullable=False)
+
+    def __repr__(self):
+        return f'<File {self.filename}>'
