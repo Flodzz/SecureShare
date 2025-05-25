567d1efa4dae936b48ae66ffd2ee3e66e79e7cbc
Hayden <107214489+Flodzz@users.noreply.github.com>
2025-05-25 23:02:49 +0100

    Project init (#6)
    * Add flask to requirements
    
    * Update .gitignore
    
    * Fix file name
    
    * Initialise route files
    
    * Update .gitignore
    
    * Add more initial project files
    
    * Register blueprints inside main init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,20 @@
 from flask import Flask
-from config import Config
+
+from .config import Config
 from .db import db
+from .routes import register_blueprints
 
 
-def create_app(config_class=Config):
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(config_class)
+    app.config.from_object(Config)
+
 
     db.init_app(app)
 
     with app.app_context():
         db.create_all()
+
+    register_blueprints(app)
 
     return app
