6e349ef434b524f68f79b4df8616f19cc42f2942
Hayden Moseley <hwlodarz@hotmail.com>
2025-05-27 16:28:19 +0100

    Implement flask migrate to add models to database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_migrate import Migrate
 from config import Config
 from .db import db
 from .routes import register_blueprints
+
+migrate = Migrate()
 
 
 def create_app(config_class=Config):
@@ -10,8 +13,9 @@ def create_app(config_class=Config):
 
     db.init_app(app)
 
-    with app.app_context():
-        db.create_all()
+    from app.models import user, file, logs
+
+    migrate.init_app(app, db)
 
     register_blueprints(app)
 
